377ec6db6ff3be3df0223e0b8ad4f52065491441
Stephen Hollmann <87396185+shollmannr@users.noreply.github.com>
2021-09-14 10:18:59 -0400

    Update Results.js

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,8 +1,8 @@
 import { Quiz } from "./Quiz"
 
-render() {
-    const{question, options, currentIndex, userAnswer, quizEnd} = this.state
-    if(quizEnd) {
+render(); {
+    const { question, options, currentIndex, userAnswer, quizEnd } = this.state
+    if (quizEnd) {
         return (
             <div>
                 <h1>Game Over. Final score is {this.state.score} points</h1>
@@ -11,46 +11,46 @@ render() {
                     {QuizData.map((item, index) => (
                         <li className='options'
                             key={index}>
-                                {item.answer}
+                            {item.answer}
                         </li>
                     ))}
                 </ul>
             </div>
         )
     }
-    
+
     return (
         <div>
             <h2>{question}</h2>
             <span>{'Question ${currentIndex + 1} of ${QuizData.length}'}</span>
             {
-                options.map(option => 
-                    <p key = {option.id} className = {'options ${userAnswer === option? "selected" : null}'}
-                    onClick = {() => this.checkAnswer(option)}>
-                    {option}
+                options.map(option =>
+                    <p key={option.id} className={'options ${userAnswer === option? "selected" : null}'}
+                        onClick={() => this.checkAnswer(option)}>
+                        {option}
                     </p>
-                    )
+                )
             }
 
-            
-            {currentIndex < QuizData.length - 1 && 
-            <button disabled = {this.state.disabled} onClick = {this.nextQuestionHander}>
-                Next Question
-            </button>}
-            {currentIndex === QuizData.length - 1 && 
-            <button onClick = {this.finishHandler} disabled = {this.state.disabled}>
-                Finish
-            </button>}
+
+            {currentIndex < QuizData.length - 1 &&
+                <button disabled={this.state.disabled} onClick={this.nextQuestionHander}>
+                    Next Question
+                </button>}
+            {currentIndex === QuizData.length - 1 &&
+                <button onClick={this.finishHandler} disabled={this.state.disabled}>
+                    Finish
+                </button>}
         </div>
     )
 }
-}
+
 
 // responds to the click of the finish button
-finishHandler =() => {
-    if(this.state.currentIndex === QuizData.length -1){
+finishHandler = () => {
+    if (this.state.currentIndex === QuizData.length - 1) {
         this.setState({
-            quizEnd:true
+            quizEnd: true
         })
     }
 }
